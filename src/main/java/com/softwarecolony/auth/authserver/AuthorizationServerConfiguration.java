bfe2fa7f245893cc4c470f2442a92c56a2d29af1
David McLoughlin <dmcloughlin@gmail.com>
2017-12-20 22:34:18 +0000

    Added the first draft of the auth-server that will look after the JWT tokens and grant types.

---
+++ src/main/java/com/softwarecolony/auth/authserver/AuthorizationServerConfiguration.java
@@ -0,0 +1,50 @@
+package com.softwarecolony.auth.authserver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+/**
+ * Created by dmcloughlin on 20/12/2017.
+ * Defines the oauth configuration.
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired // -> the users configuration.
+    private AuthenticationManager authenticationManager;
+
+    @Bean
+    public TokenStore tokenStore() {
+
+        return new InMemoryTokenStore();
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+        endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+        clients.inMemory()
+                .withClient("live-test")
+                .secret("bG12ZS10ZXN0") // We won't use this with our Angular app!
+                .authorizedGrantTypes("password")
+                .scopes("webapp")
+                .accessTokenValiditySeconds(3600);
+    }
+
+
+}
---
+++ src/main/java/com/softwarecolony/auth/authserver/WebSecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.softwarecolony.auth.authserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by dmcloughlin on 20/12/2017.
+ * Defines the user's configuration.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("USER")
+                .and()
+                .withUser("manager")
+                .password("password")
+                .credentialsExpired(true)
+                .accountExpired(true)
+                .accountLocked(true)
+                .authorities("WRITE_PRIVILEGES", "READ_PRIVILEGES")
+                .roles("MANAGER");
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
