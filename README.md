dde7ca9292bcf886f16a4dc53789801ea7c8350e
David McLoughlin <dmcloughlin@gmail.com>
2017-12-21 00:11:34 +0000

    Updated the README.md with Getting started instructions and added a dummy default plaintext password encoder while we are in the prototyping phase.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # auth-server
 Authentication server implementation serving various grant types
+
+## Getting started
+### 1) Clone the repository
+```html
+git clone https://github.com/dmcloughlin/auth-server.git
+```
+
+### 2) Start spring boot
+``` ./mvnw spring-boot:run ``` on Mac/Linux
+
+``` mvn spring-boot:run ``` on Windows
+
+### 3) Configure Postman
+Enter HTTP Basic Authentication credentials: (These represent client and secret)
+```html
+username: live-test
+password: bG12ZS10ZXN0
+```
+
+### 4) Issue request for Token
+Enter the following in the URL in Postman
+```html
+http://localhost:8080/oauth/token?grant_type=password&client_id=live-test&username=user&password=password
+```
+
+You should see something like the following:
+
+```html
+{
+    "access_token": "00c7ccf6-85a8-47cf-9bdd-e9e08dc3609a",
+    "token_type": "bearer",
+    "expires_in": 3599,
+    "scope": "webapp"
+}
+```
+
+
+
+
+
+
+
+
--- src/main/java/com/softwarecolony/auth/authserver/WebSecurityConfiguration.java
+++ src/main/java/com/softwarecolony/auth/authserver/WebSecurityConfiguration.java
@@ -3,9 +3,13 @@ package com.softwarecolony.auth.authserver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * Created by dmcloughlin on 20/12/2017.
@@ -15,25 +19,39 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password("password")
-                .roles("USER")
-                .and()
-                .withUser("manager")
-                .password("password")
-                .credentialsExpired(true)
-                .accountExpired(true)
-                .accountLocked(true)
-                .authorities("WRITE_PRIVILEGES", "READ_PRIVILEGES")
-                .roles("MANAGER");
-    }
-
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
+
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return charSequence.toString();
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String s) {
+                return true;
+            }
+        };
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        final InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build());
+        return manager;
     }
 }
